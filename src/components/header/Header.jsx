f9e4a704cfb80be0580ed66ccf69a6e21c1fe6dc
SMurielle <a.muriellesarah@gmail.com>
2023-05-22 16:25:01 +0100

    added header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,21 +3,22 @@ import './header.css'
 import './CTA'
 import CTA from './CTA' //here we import the CTA component inside our header
 import HeaderSocial from './HeaderSocial' //import of headerSocial component
+import me from '../../assets/me.jpg'
 export const Header = () => {
-return (
-<header>
-<div className='container header__container'>
-<h5>Hello I'm</h5>
-<h1>Eyong Oscar Enownyuo</h1>
-<h5 className='text-light'>Engineering student</h5>
-<CTA/>
-<HeaderSocial/>                                         
-<div className='my_image'>
-<img src={me} alt='my image'/>
-</div>
-<a href='#contact' className='scroll__down'>Scroll down</a>
-</div>
-</header>
-)
+    return (
+        <header>
+            <div className='container header__container'>
+                <h5>Hello I'm</h5>
+                <h1>Kombou Komga Murielle Nahomy</h1>
+                <h5 className='text-light'>Engineering student</h5>
+                <CTA />
+                <HeaderSocial />
+                <div className='my_image'>
+                    <img src={me} alt='my pic' />
+                </div>
+                <a href='#contact' className='scroll__down'>Scroll down</a>
+            </div>
+        </header>
+    )
 }
 export default Header;
